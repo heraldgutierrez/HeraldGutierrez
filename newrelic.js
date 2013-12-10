7588e87634a32c9799e76e7e628cfce3c64c4296
Herald Gutierrez <heraldgutierrez@hotmail.com>
2013-12-10 10:20:29 -0600

    added newrelic to package.json

--- newrelic.js
+++ newrelic.js
@@ -8,7 +8,7 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name : ['Herald Gutierrez'],
+  app_name : ['application-name'],
   /**
    * Your New Relic license key.
    */
