57b07c126518d9576d083eed93b1e68061c95774
Herald Gutierrez <herald.gutierrez@peoplecorporation.com>
2017-04-04 09:06:39 -0500

    Layout - Added ".active" class to navigation based on scrolling

--- public/scripts/Controllers/site-controller.js
+++ public/scripts/Controllers/site-controller.js
@@ -8,11 +8,39 @@ app.controller('siteController', ['$scope', '$window', function($scope, $window)
 		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
 	});
 
+
+	// Get all navigation links that link to a section and store as a list
+	var navigationLinks = $("nav li a[href*=#]");
+	var navigationLinkIDs = [];
+	for (var i = 0; i < navigationLinks.length; i++) {    
+		var ahref = $(navigationLinks[i]).attr('href');
+		navigationLinkIDs.push(ahref);
+	}
+
 	angular.element($window).bind("scroll", function(e) {
-	    if ($window.pageYOffset > 1) {
+		// Display navigation when the window has been scrolled down
+		if ($window.pageYOffset > 1) {
 			$('header').fadeIn(500);
 		} else {
 			$('header').fadeOut(500);
+		}
+
+		// Add the 'active' class to the navigation when we are viewing the appropiate section
+		var windowPosition = $(window).scrollTop(); 	// Get the offset of the window from the top of page
+		var windowHeight = $(window).height(); 			// Get the height of the window
+		var sectionBuffer = windowHeight * 0.25;		// Add .active class once the section has reached 25% from the top of the window
+		var docHeight = $(document).height();
+
+		for (var i = 0; i < navigationLinkIDs.length; i++) {
+			var sectionID = navigationLinkIDs[i];
+			var sectionPosition = $(theID).offset().top - sectionBuffer; // Get the offset of the section from the top of page - the buffer
+			var sectionHeight = $(theID).height(); 		// Get the height of the section in question
+
+			if (windowPosition >= sectionPosition && windowPosition < (sectionPosition + sectionHeight)) {
+				$("a[href='" + sectionID + "']").addClass("active");
+			} else {
+				$("a[href='" + sectionID + "']").removeClass("active");
+			}
 		}
 	})
 }]);
