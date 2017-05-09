750b1e345532287c9b8f09809f7ae7cf5250b954
Herald Gutierrez <herald.gutierrez@peoplecorporation.com>
2017-05-09 15:15:42 -0500

    Revert "App.js - Delete commented code"
    This reverts commit f656c8c7769eefefa4ab4ca7108c4944e0fce0a3.

--- app.js
+++ app.js
@@ -48,3 +48,114 @@ server.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
 
+
+// /**
+//  * Module dependencies.
+//  */
+
+// var express = require('express');
+// var http = require('http');
+// var path = require('path');
+// var app = express();
+// var newrelic = require('newrelic');
+// var sass = require('node-sass');
+
+// // routes
+// var routes = require('./routes');
+
+// // all environments
+// app.configure(function() {
+// 	app.set('port', process.env.PORT || 5000);
+// 	app.set('views', __dirname + '/views');
+// 	app.set('view engine', 'jade');
+
+// 	app.use(
+// 		sass.middleware({
+// 			src: __dirname + '/public/sass',
+// 			dest: __dirname + '/public',
+// 			debug: false
+// 		})
+// 	);
+
+// 	app.use(express.favicon(path.join(__dirname, 'public/img/favicon.ico')));
+// 	app.use(express.logger('dev'));
+// 	app.use(express.bodyParser());
+// 	app.use(express.methodOverride());
+// 	app.use(express.cookieParser('your secret here'));
+// 	app.use(express.session());
+// 	app.use(express.static(path.join(__dirname, 'public')));
+// 	app.use(app.router);
+
+// 	// Error
+// 	app.use(function(req, res, next){
+// 		res.status(404);
+
+// 		// respond with html page
+// 		if (req.accepts('html')) {
+// 			res.render('404', { 
+// 				url: req.url 
+// 			});
+// 			return;
+// 		}
+
+// 		// respond with json
+// 		if (req.accepts('json')) {
+// 			res.send({ error: 'Not found' });
+// 			return;
+// 		}
+
+// 		// default to plain-text. send()
+// 		res.type('txt').send('Not found');
+// 	});
+// });
+
+// // development only
+// app.configure('development', function() {
+// 	app.use(express.errorHandler());
+// });
+
+
+
+// /*********************************************************
+// **********************************************************
+// *
+// *	HTML pages/handlers
+// *
+// *********************************************************
+// *********************************************************/
+
+// app.get('/', routes.index);
+// app.get('/Portfolio', routes.portfolio);
+// app.get('/Resume', routes.resume);
+
+// // app.get('/project', function(req, res) {
+// // 	res.render('project', {
+// // 		otherInfo : '<h1>text</h1>'
+// // 	});
+// // });
+
+// // Scoreboard
+// app.get('/Portfolio/Scoreboard', routes.scoreboard);
+// app.get('/Portfolio/Scoreboard/Simple', routes.simpleScoreboard);
+// app.get('/Portfolio/Scoreboard/LED', routes.LEDScoreboard);
+// app.get('/Portfolio/Scoreboard/LEDSimple', routes.LEDSimple);
+
+
+// // Cards
+// app.get('/Portfolio/Cards', routes.Cards);
+
+// // Casino
+// app.get('/Portfolio/Casino/VideoPoker', routes.videoPoker);
+
+// // GymLocker
+// app.get('/Portfolio/GymLocker', routes.GymLocker);
+
+// // MIPS
+// app.get('/Portfolio/MIPS', routes.MIPS);
+
+// // Number Munchers
+// app.get('/Portfolio/NumberMunchers', routes.NumberMunchers);
+
+// http.createServer(app).listen(app.get('port'), function(){
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
